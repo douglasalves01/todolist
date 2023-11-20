1340d1834c3d083885156e82099622d72abb69b6
Douglas Alves <douglasmonttozo@hotmail.com.br>
2023-11-20 01:12:02 -0300

    adicionando cadastro de usuarios

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,10 +1,29 @@
 import express from "express";
+import { criarUsuario, retornarDados, validarSenha } from "../db/conn.js";
 
 export class AuthController {
   static login(req, res) {
     res.render("auth/login");
   }
   static async loginPost(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const sql = `SELECT * FROM USERS WHERE EMAIL = '${email}'`;
+    const result = await retornarDados(sql, []);
+    if (result.length === 0) {
+      return res.status(422).json({ msg: "Por favor, utilize outro e-mail" });
+    } else {
+      let dados;
+      if (result) {
+        dados = result.map((item) => ({
+          id: item[0],
+          userPassword: item[2],
+        }));
+      }
+      dados.forEach((item) => {
+        validarSenha(req, res, password, item.userPassword, item.id);
+      });
+    }
     res.render("auth/login");
   }
   static register(req, res) {
@@ -16,9 +35,18 @@ export class AuthController {
     const confirmPassword = req.body.confirmPassword;
     try {
       if (password != confirmPassword) {
-        console.log("As senhas diferem!");
+        return res.status(422).json({ msg: "As senhas não conferem!" });
       }
-    } catch (error) {}
+      const sql = `SELECT * FROM USERS WHERE EMAIL = '${email}'`;
+      const result = await retornarDados(sql, []);
+      if (result.length === 0) {
+        criarUsuario(email, password);
+      } else {
+        return res.status(422).json({ msg: "Por favor, utilize outro e-mail" });
+      }
+    } catch (error) {
+      console.log(error);
+    }
     res.render("auth/register");
   }
 }
--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,7 @@
 import oracledb from "oracledb";
+import bcryptjs from "bcryptjs";
 import * as dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 
 dotenv.config();
 
@@ -8,3 +10,61 @@ export let conn = oracledb.getConnection({
   password: process.env.ORACLE_PASSWORD,
   connectString: process.env.ORACLE_STR,
 });
+export async function criarUsuario(email, password) {
+  try {
+    const salt = await bcryptjs.genSalt(12);
+    let passwordHash = await bcryptjs.hash(password, salt);
+    const sql = `INSERT INTO USERS 
+       (ID_USER, EMAIL, PASSWORD )
+       VALUES
+       (SEQ_USER.NEXTVAL, :1, :2)`;
+    const dados = [email, passwordHash];
+    let resSql = await (await conn).execute(sql, dados);
+
+    await (await conn).commit();
+
+    const rowsInserted = resSql.rowsAffected;
+    if (rowsInserted !== undefined && rowsInserted === 1) {
+      console.log("Dados inseridos");
+    } else if (rowsInserted === undefined) {
+      console.log("Nenhum dado inserido");
+    }
+  } catch (error) {
+    console.log("Erro na execução do sql", error);
+  }
+}
+export async function retornarDados(sql, dados) {
+  try {
+    let resSql = await (await conn).execute(sql, dados);
+    return resSql.rows;
+  } catch (e) {
+    console.log("erro na consulta sql", cr);
+  }
+}
+export async function validarSenha(req, res, password, userPassword, id) {
+  const checkPassword = await bcryptjs.compare(password, userPassword);
+  if (!checkPassword) {
+    return res.status(422).json({ msg: "Senha inválida" });
+  }
+  try {
+    const secret = process.env.SECRET;
+    const token = jwt.sign({ id: id }, secret);
+    res.status(200).json({ msg: "Autenticação reaizada com sucesso", token });
+  } catch (err) {
+    console.log(err);
+  }
+}
+export function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ msg: "Acesso negado" });
+  }
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+    next();
+  } catch (error) {
+    res.status(400).json({ msg: "Token inválido" });
+  }
+}
